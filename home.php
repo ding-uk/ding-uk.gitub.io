deb40368eeacab912d0de881a6d88eb0a8514ce5
Daniel Gibson <soulninetynine@gmail.com>
2016-11-22 15:30:56 +0700

    Editing content

--- home.php
+++ home.php
@@ -6,10 +6,9 @@
   $pageTitle = "DING";
   include("inc/header.php"); ?>
 <div class="content">
- <img src="images/ding.png" class="me">
   <div class="intro">
     <p>My aim has always been to make my thoughts visible or audible.<br>Arranging bits and pieces into a way which is pleasurable for the experiencer.<br>Experimenting with colours, faces, shapes, places and sounds.<br>
-    I am currently looking for work as a junior developer with an inspiring start up company.  I have been teaching my self HTML, CSS, Javascript and Ruby on Rails.</p>
+    I am currently looking for work as a junior developer with an inspiring start up company.  I have been teaching my self HTML, CSS, Javascript, PHP and Ruby on Rails.</p>
     <p>Please check my work.  If you like what you see make sure to give me a email -->
   </div>
 
