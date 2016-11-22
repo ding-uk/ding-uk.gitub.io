cf9b48ba81c199f48b6f95d5624581f30c307db1
Daniel Gibson <soulninetynine@gmail.com>
2016-11-22 15:23:13 +0700

    Editing intro page

--- home.php
+++ home.php
@@ -2,11 +2,16 @@
   <p>This whole world is wild at heart and weird on top.</p>
 </div>
 
-<div class="content">
   <?php
   $pageTitle = "DING";
   include("inc/header.php"); ?>
-
+<div class="content">
+ <img src="images/ding.png" class="me">
+  <div class="intro">
+    <p>My aim has always been to make my thoughts visible or audible.<br>Arranging bits and pieces into a way which is pleasurable for the experiencer.<br>Experimenting with colours, faces, shapes, places and sounds.<br>
+    I am currently looking for work as a junior developer with an inspiring start up company.  I have been teaching my self HTML, CSS, Javascript and Ruby on Rails.</p>
+    <p>Please check my work.  If you like what you see make sure to give me a email -->
+  </div>
 
 
 </div>
--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+/* Top quote */
+
 $(window).scroll(function() {
         var scroll = $(window).scrollTop();
             $('.quote, .slogan').css({'opacity':(( 350-scroll )/350)+0.1}
 					);
         });
 
-				window.addEventListener("scroll", function() {
-				    if (window.scrollY > 400) {
-				        $('.nav').fadeIn(1500);
+/* Nav and Into */
+
+window.addEventListener("scroll", function() {
+	if (window.scrollY > 400) {
+		$('.nav, .intro, .me').fadeIn(1500);
+			}
+				else {
+				  $('.nav, .intro, .me').fadeOut();
 				    }
-				    else {
-				        $('.nav').fadeOut();
-				    }
-				},false);
+		},false);
+
+
+		$(function(){
+		    $.fn.extend({
+		      slideInLeft: function() {
+		        return this.each(function() {
+		            $(this).show().css({opacity: 0, position: 'relative', left: "-200px"});
+		            $(this).animate({opacity: 1, left: 0}, 'slow');
+		        });
+		      }
+		    });
+
+		    $(".me").show().lazyload({
+		        'effect': 'slideInLeft'
+		    });
+		});
